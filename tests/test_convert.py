c28bf6329d54165a23a2f1b9a8a22d8b15488d0c
sacha <sacha.schutz@pm.me>
2024-05-24 00:53:04 +0200

    add test for counting row

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -12,8 +12,27 @@ def test_conversion():
 
 	sqlite_to_duckdb(sqlite_path, duckdb_path)
 
-	
-	
+
+def test_count():
+		
+	duckdb_path = utils.generate_fake_duckdb()
+	sqlite_path = utils.generate_fake_sqlite()
+
+	sqlite_to_duckdb(sqlite_path, duckdb_path)
+
+
+	d_conn = duckdb.connect(duckdb_path)
+	s_conn = sqlite3.connect(sqlite_path)
+
+	for table in d_conn.sql("SHOW TABLES").fetchall():
+		table_name = table[0]
+
+		query = f"SELECT COUNT(*) FROM {table_name}"
+
+		duckdb_count = d_conn.sql(query).fetchone()
+		sqlite_count = s_conn.execute(query).fetchone()
+
+		assert duckdb_count == sqlite_count
 
 	
 	
