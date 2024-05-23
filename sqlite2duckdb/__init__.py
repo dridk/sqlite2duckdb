51557a92136845a6edbabe2acc76862c86140e1e
sacha <sacha.schutz@pm.me>
2024-05-24 00:01:31 +0200

    blackify

--- sqlite2duckdb/__init__.py
+++ sqlite2duckdb/__init__.py
@@ -1,3 +1,4 @@
 import importlib.metadata
 from sqlite2duckdb.sqlite_to_duckdb import sqlite_to_duckdb
-__VERSION__ = importlib.metadata.version('sqlite2duckdb')
+
+__VERSION__ = importlib.metadata.version("sqlite2duckdb")
--- sqlite2duckdb/sqlite_to_duckdb.py
+++ sqlite2duckdb/sqlite_to_duckdb.py
@@ -1,39 +1,41 @@
 import duckdb
 import os
 import sqlite3
-import time 
+import time
 
-def sqlite_to_duckdb(sqlite_db:str, duck_db:str):
 
+def sqlite_to_duckdb(sqlite_db: str, duck_db: str):
     print(f"Create {duck_db} databases")
 
     if not os.path.exists(sqlite_db):
         raise Exception(f"File {sqlite_db} doesn't exists")
 
-    # Remove target db if exists
+    # Remove target db if exists
     if os.path.exists(duck_db):
         raise Exception(f"Database {duck_db} already exists")
 
-    # Create databases 
+    # Create databases
 
     start_time = time.perf_counter()
     conn = duckdb.connect(duck_db)
     db_name = conn.sql("SELECT database_name FROM duckdb_databases").fetchone()[0]
 
     ## Install sqlite
-    conn.sql(f"""
+    conn.sql(
+        f"""
     INSTALL sqlite;
     LOAD sqlite;
     ATTACH '{sqlite_db}' as __other;
-    """)
+    """
+    )
 
-    ## Get sqlite Names 
+    ## Get sqlite Names
     conn.sql("USE __other")
-    tables = [i[0] for i  in conn.sql("SHOW tables").fetchall()]
+    tables = [i[0] for i in conn.sql("SHOW tables").fetchall()]
     print(f"{len(tables)} tables found(s)")
     conn.sql(f"USE {db_name}")
 
-    # Create tables     
+    # Create tables
     for table in tables:
         print(f"Create duckdb table {table}")
         conn.sql(f"CREATE TABLE {table} AS select * FROM __other.{table}")
@@ -43,4 +45,3 @@ def sqlite_to_duckdb(sqlite_db:str, duck_db:str):
     end_time = time.perf_counter()
     execution_time = (end_time - start_time) * 1000
     print(f"Done in {execution_time:.2f} ms !")
-
